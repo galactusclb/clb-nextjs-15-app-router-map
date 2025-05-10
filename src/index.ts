da4efcd74af48d9a59e5933cc3ac9ab3d9ab5d8d
galactusclb <chanakabalasuriya123@gmail.com>
2025-05-10 23:29:15 +0530

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+export * from "./functions"
+export * from "./types"
+
+//? Sample Route map
+//   export const routeMap: RouteMap = {
+//     home: {
+//       path: '/home',
+//     },
+//     products: {
+//       path: '/products',
+//       details: {
+//         path: '/products/[product-id]',
+//       },
+//     },
+//     userProfile: {
+//       path: '/users/[user-id]',
+//     },
+//   };
+
+//? Sample usages
+//   getRoute(routeMap.home, {});
+//   getRoute(routeMap.products, {});
+//   getRoute(routeMap.products.details, { 'product-id': 12 });
+//   getRoute(routeMap.userProfile, { 'user-id': 21 });
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// Recursive type for a route node
+export type RouteNode = {
+    path: string;
+    [key: string]: RouteNode | string; // subdirectories keys mapped to RouteNode
+};
+
+// Top-level type for the route map
+export type RouteMap = {
+    [route: string]: RouteNode;
+};
